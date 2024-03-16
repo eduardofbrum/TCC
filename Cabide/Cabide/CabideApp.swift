cd39cb4284b6c2a2416a8efdde4bbbeeaaf77152
Gabriel-M-Martins <gabrielmmartinscontato@gmail.com>
2024-03-16 17:29:25 -0300

    created proj

---
+++ Cabide/Cabide/CabideApp.swift
@@ -0,0 +1,17 @@
+//
+//  CabideApp.swift
+//  Cabide
+//
+//  Created by Gabriel Medeiros Martins on 16/03/24.
+//
+
+import SwiftUI
+
+@main
+struct CabideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cabide/Cabide/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cabide
+//
+//  Created by Gabriel Medeiros Martins on 16/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
