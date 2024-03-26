ac0f3f25c7012e085e0ea145f6eaf253dd758412
Gabriel-M-Martins <gabrielmmartinscontato@gmail.com>
2024-03-25 21:08:49 -0300

    created DatabaseExampleView for demonstration purposes

--- Cabide/Cabide/DatabaseExampleView.swift
+++ Cabide/Cabide/DatabaseExampleView.swift
@@ -9,16 +9,20 @@ import SwiftUI
 import DependencyInjection
 import Database
 
-struct ContentView: View {
+struct DatabaseExampleView: View {
+    // This would be on the view model.
     @Injected private var repo: any Repository<Clothing>
     
+    // This would also be on the view model.
     @State private var clothes: [Clothing] = []
     
     var body: some View {
         VStack {
             Button("Create") {
                 if let img = UIImage(systemName: "pencil")?.pngData() {
-                    repo.save(Clothing(image: img)) { model in
+                    let clothing = Clothing(image: img)
+                    // Tries to save a Model (anything that conforms to EntityRepresentable). If it had any trouble, calls completion with nil.
+                    repo.save(clothing) { model in
                         if let model = model {
                             clothes.append(model)
                             print("Created and saved clothing with id \(model.id).")
@@ -30,6 +34,7 @@ struct ContentView: View {
             }
             
             Button("Retrieve") {
+                // Fetchs *all* data of type Clothing. If it has any relations, will load those aswell.
                 repo.fetch { (result: [Clothing]) in
                     clothes = result
                     print("Retrieved \(clothes.count) clothes.")
@@ -38,6 +43,7 @@ struct ContentView: View {
             
             Button("Delete all") {
                 for clothing in clothes {
+                    // Asks for a model to delete. If it has any relations, will follow the deletion rules (I think it doesn't cascade automatically, only 2023 Gabriel knows wtf this actually does).
                     self.repo.delete(clothing)
                 }
                 
@@ -49,5 +55,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    DatabaseExampleView()
 }
